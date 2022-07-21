9c57a6a815a7a0dd6f5d56a05428981a70e4deac
Jerry Wwang <doghost111@gmail.com>
2022-07-21 17:02:07 +0800

    index api 路由更新

--- index.js
+++ index.js
@@ -10,14 +10,14 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 app.use(express.json());
 
-app.use("/suyoung", require(__dirname + "/routes/index"));
-app.use("/suyoung/booking", require(__dirname + "/routes/booking"));
-app.use("/suyoung/member", require(__dirname + "/routes/member"));
-app.use("/suyoung/join", require(__dirname + "/routes/join"));
-app.use("/suyoung/SuMap", require(__dirname + "/routes/SuMap"));
-app.use("/suyoung/Cart", require(__dirname + "/routes/Cart"));
-app.use("/suyoung/recipes", require(__dirname + "/routes/recipes"));
-app.use("/suyoung/act", require(__dirname + "/routes/act"));
+app.use("/", require(__dirname + "/routes/index"));
+app.use("/booking", require(__dirname + "/routes/booking"));
+app.use("/member", require(__dirname + "/routes/member"));
+app.use("/join", require(__dirname + "/routes/join"));
+app.use("/SuMap", require(__dirname + "/routes/SuMap"));
+app.use("/Cart", require(__dirname + "/routes/Cart"));
+app.use("/recipes", require(__dirname + "/routes/recipes"));
+app.use("/act", require(__dirname + "/routes/act"));
 
 app.use((req, res) => {
     res.send(`<h2>找不到頁面 404</h2>`);
