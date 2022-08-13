d609556ddaadd0ce49adb652da8bfcdf791ae899
“terry19970422” <terry19970422@outlook.com>
2022-08-13 10:16:06 +0800

    新增email api 與 訂單api

--- routes/Cart.js
+++ routes/Cart.js
@@ -5,22 +5,8 @@ const db = require(__dirname + "../../modules/mysql-connect");
 const router = express.Router();
 const bodyParser = require("body-parser");
 const cors = require("cors");
-//設定假使用者
-const fake_User = 5;
-
-const getUserCart =async(user_id)=>{
-
-    const sql = `SELECT r.*, c.quantity 
-    FROM carts c 
-    JOIN room r 
-    ON c.room_id=r.sid 
-    WHERE user_id=?
-    ORDER BY c.created_at`;
-
-    const [r] = await db.query(sql,[user_id]);
-
-    return r;
-}
+const nodemailer = require("nodemailer");
+const OrderList = require(__dirname + '/../modules/OrderList');
 
 //extended： 使用 qs 進行解析，若為 false，則採用 querystring 進行解析，預設為 true
 router.use(bodyParser.urlencoded({ extended: false }));
@@ -29,17 +15,64 @@ router.use(cors());
 // 轉議json 資料
 router.use(express.json());
 
+//=================nodeamiler
+//send email
+function sendEmail(email1) {
+    var email1 = "shinderr0125@gmail.com";
+
+    var mail = nodemailer.createTransport({
+        host: "smtp.gmail.com",
+        port: 465,
+        auth: {
+            user: "shinderr0125@gmail.com",
+            pass: "zzukvqfcndhvbcrh",
+        },
+    });
+
+    var mailOptions = {
+        from: "shinderr0125@gmail.com",
+        to: email1,
+        subject: "shuyoung舒營 訂單編號",
+        html: `<h1>感謝預訂</h1>`,
+    };
+
+    mail.sendMail(mailOptions, function (error, info) {
+        if (error) {
+            console.log("flase");
+        } else {
+            console.log("success");
+        }
+    });
+}
+
+//================nodemailer
+
+router.post("/orderDetail-email", async (req, res, next) => {
+    var email = "shinderr0125@gmail.com";
+
+    var order_id = req.body.order_id;
+
+    console.log(email);
+
+        res.json({
+            success: true,
+            message: "驗證成功，正在寄送電子郵件...",
+            code: "200",
+        });
+
+        var sent = sendEmail(email, order_id);
+
+        if (sent != "0") {
+            console.log("信件已成功寄出");
+        }
+});
+
 //CRUD
 //將商品加入購物車
-router.post('/order/item/add', async(req,res)=>{
+router.post('/order/add', async(req,res)=>{
     const userId = await req.params.userId;
 
     console.log(userId);
-    //判斷有沒有商品
-    // const output = {
-    //     success : false,
-    //     error : ''
-    // }
     let output ={
         success: false,
         error:'',
@@ -63,41 +96,18 @@ router.post('/order/item/add', async(req,res)=>{
         return  res.json(output);
     }
 
-    // const sql = `SELECT * FROM room WHERE sid=? `;
-    // const [r1] = await db.query(sql, [req.body.room_id]);
-    // if(!r1.length){
-    //     output.error = '沒有此房型'
-    //     return  res.json(output);
-    // }
-
-    // `member_id`, `room_id`, `room_type_id`, `num_adults`, `num_children`, `Booking_Date`, `price`, `start_date`, `end_date`
-    // for (let item of req.body.orderItems){
-    //     const sql1 = "INSERT INTO `order_pitems` SET ?";
-    //     const [results] = await db.query(sql1, [item]);
-    //     console.log(req.body)
-    // }
-
-    // const sql2 = "INSERT INTO `room_reservation`(`member_id`, `room_id`, `room_type_id`, `num_adults`, `num_children`, `Booking_Date`, `price`, `start_date`, `end_date`) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
-
-    // const [r2] = await db.query(sql2,[req.body.orderItems.member, req.body.orderItems.room_id, req.body.orderItems.roomType, req.body.orderItems.adults, req.body.orderItems.children, req.body.orderItems.bd, req.body.orderItems.price, req.body.orderItems.sd, req.body.orderItems.ed]);
-
     for (let item of req.body.orderItems){
         const sql2 = "INSERT INTO `room_reservation` SET ?";
         const [r2] = await db.query(sql2, [item]);
         console.log(req.body)
     }
 
-    output = {...output, body: req.body.orderInfo};
+    output = {...output, body: req.body.orderItems};
     res.json(output);
-
-    // if(r2.affectedRows){
-    //     output.success = true;
-    // }
-    // res.json(output);
 });
 
 //寫入信用卡資訊
-router.post('/order/card/add', async(req,res)=>{
+router.post('/card/add', async(req,res)=>{
     let output ={
         success: false,
         error:'',
@@ -117,63 +127,32 @@ router.post('/order/card/add', async(req,res)=>{
     }
     res.json(output); 
 });
-    
 
-//取得商品列表'
-router.get('/', async(req,res)=>{
-    res.json(await getUserCart(fake_User));
+//寫入訂單資訊
+router.post('/orderDetail/add', async(req,res)=>{
+    let output ={
+        success: false,
+        error:'',
+        insertId: 0,
+    };
+    console.log(req.body.orderDetail)
+
+    for (let item of req.body.orderDetail){
+        const sql3 = "INSERT INTO `order_detail` SET ?";
+        const [r3] = await db.query(sql3, [item]);
+        console.log(req.body.orderDetail)
+    }
+
+    output = {...output, body: req.body.orderDetail};
+    res.json(output);
 });
 
-//修改
-router.put('/', async(req,res)=>{
-//room_sid,quan
-const output = {
-    success: false,
-    error: "",
-};
-if (!req.body.room_id || !req.body.quantity) {
-    output.error = "參數不足";
-    return res.json(output);
-}
+//取得訂單內容
+router.get('/:orderId', async (req, res)=>{
 
-if (+req.body.quantity < 1) {
-    output.error = "數量不能小於 1";
-    return res.json(output);
-}
+    let p = await OrderList.getOrderById(req.params.orderId)
 
-// 判斷該商品是否已經加入購物車
-const sql3 = `SELECT COUNT(1) num FROM carts WHERE room_id=? AND user_id=?`;
-const [[{ num }]] = await db.query(sql3, [req.body.room_id, fake_User]);
-if (num <= 0) {
-    output.error = "購物車內沒有這項商品";
-    return res.json(output);
-}
-
-
-const sql2 = "UPDATE `carts` SET `quantity`=? WHERE room_id=? AND user_id=?";
-const [r2] = await db.query(sql2, [
-    req.body.quantity,
-    req.body.room_id,
-    fake_User,
-]);
-output.r2 = r2;
-
-if (r2.affectedRows && r2.changedRows) {
-    output.success = true;
-}
-
-output.cart = await getUserCart(fake_User);
-res.json(output);
-
-});
-
-//刪除
-router.delete('/', async(req,res)=>{
-//room_id
-    const sql = `DELETE FROM carts WHERE user_id=? AND room_id=?`;
-    await db.query(sql, [fake_User, req.body.room_id]);
-
-    res.json(await getUserCart(fake_User));
+    res.json(p);
 });
 
 // router 一定要回傳module
